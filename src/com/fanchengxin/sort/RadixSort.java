cef1ab43c68bfea8fe218836fc94f0659ded3483
frozencheng <391920971@qq.com>
2021-01-27 14:18:05 +0800

    排序算法=====>基数排序(添加可以对含有负数的数组进行排序)

--- src/com/fanchengxin/sort/RadixSort.java
+++ src/com/fanchengxin/sort/RadixSort.java
@@ -13,6 +13,10 @@ public class RadixSort {
         sort(arr);
         System.out.println(Arrays.toString(arr));
         System.out.println("=======================");
+        int[] arr2 = {2, -99, 0, 1, 99, 444, 323, 789, -22, -1000};
+        sort2(arr2);
+        System.out.println(Arrays.toString(arr2));
+        System.out.println("=======================");
         int[] array = new int[80000000];
         for (int i = 0; i < array.length; i++) {
             array[i] = (int) (Math.random() * 80000);
@@ -58,6 +62,60 @@ public class RadixSort {
                     indexHelper++;
                     bucketEleCount[i]--;
                     j++;
+                }
+            }
+        }
+    }
+
+    /**
+     * 对包含负数的数组进行排序
+     */
+    public static void sort2(int[] arr) {
+
+        int[][] bucket = new int[10][arr.length];
+        int[] bucketEleCount = new int[bucket.length];
+        int temp = Math.abs(arr[0]); // 临时变量 假设索引0的值位最大值（绝对值）
+        int indexHelper;
+        // 遍历 比较 来获取最大值(绝对值)
+        for (int i = 1; i < arr.length; i++) {
+            if (temp < Math.abs(arr[i])) {
+                temp = Math.abs(arr[i]);
+            }
+        }
+        String str = temp + "";
+        int element;
+        for (int k = 0, n = 1; k < str.length(); k++, n *= 10) {
+            for (int value : arr) {
+                element = value / n % 10; // 当前第K次循环 arr中元素的基数
+                // 对element 判断是否位负数 如果为负数就去绝对
+                element = Math.abs(element);
+                bucket[element][bucketEleCount[element]] = value; // 将arr[i] 放入对应的桶中
+                bucketEleCount[element]++; // 对放入对应桶的数据进行基数
+            }
+            // 将bucket中的数据依次插入到arr中
+            int j = 0;
+            // 先将桶从大到小将负数 添加  然后在从小到大添加正数
+            for (int i = bucketEleCount.length - 1; i >= 0; i--) {
+                indexHelper = 0;
+                while (bucketEleCount[i] != 0 && indexHelper < bucketEleCount.length) {
+                    if (bucket[i][indexHelper] < 0) {
+                        arr[j] = bucket[i][indexHelper];
+                        j++;
+                        bucketEleCount[i]--;
+                    }
+                    indexHelper++;
+                }
+            }
+            // 然后将正数添加
+            for (int i = 0; i < bucketEleCount.length; i++) {
+                indexHelper = 0;
+                while (bucketEleCount[i] != 0 && indexHelper < bucketEleCount.length) {
+                    if (bucket[i][indexHelper] >= 0) {
+                        arr[j] = bucket[i][indexHelper];
+                        j++;
+                        bucketEleCount[i]--;
+                    }
+                    indexHelper++;
                 }
             }
         }
